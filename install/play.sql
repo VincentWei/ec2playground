4b834f2a147fe5e79cf614cb09e661628b246b8e
Vincent Wei <vincent@minigui.org>
2025-01-06 10:47:08 +0800

    update tables

--- install/play.sql
+++ install/play.sql
@@ -26,34 +26,59 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 --
 
 CREATE TABLE `users` (
-  `id` int(4) UNSIGNED NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `passwd` varchar(255) NOT NULL,
-  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `lastSignIn` datetime NOT NULL
+  `id` BIGINT UNSIGNED NOT NULL,
+  `name` VARCHAR(256) NOT NULL,
+  `passwd` VARCHAR(256) NOT NULL,
+  `createdAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `lastSignIn` DATETIME NOT NULL
 ) ENGINE=InnoDB;
 
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `name` (`name`);
+  ADD UNIQUE KEY `name` (`name`),
+  ADD KEY `createdAt` (`createdAt`);
 
 --
 -- Table structure for table `snippets`
 --
 
 CREATE TABLE `snippets` (
-  `id` bigint(8) UNSIGNED NOT NULL,
-  `digest` varchar(32) NOT NULL,
-  `userId` int(4) UNSIGNED NOT NULL,
-  `section` varchar(32) NOT NULL,
-  `title` varchar(32) NOT NULL,
-  `gitlabId` int(4) UNSIGNED NOT NULL
+  `id` BIGINT UNSIGNED NOT NULL,
+  `digest` VARCHAR(32) NOT NULL,
+  `userId` BIGINT UNSIGNED NOT NULL,
+  `section` VARCHAR(256) NOT NULL,
+  `title` VARCHAR(256) NOT NULL,
+  `gitlabId` BIGINT UNSIGNED NOT NULL,
+  `score` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+  `nrStars` BIGINT UNSIGNED NOT NULL DEFAULT 0,
+  `nrComments` BIGINT UNSIGNED NOT NULL DEFAULT 0,
+  `state` VARCHAR(32) NOT NULL DEFAULT '',
+  `createdAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB;
 
 ALTER TABLE `snippets`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `digest` (`digest`),
-  ADD KEY `userId` (`userId`);
+  ADD KEY `userId` (`userId`),
+  ADD KEY `createdAt` (`createdAt`),
+  ADD KEY `updatedAt` (`updatedAt`);
+
+--
+-- Table structure for table `selectedSnippets`
+--
+
+CREATE TABLE `selectedSnippets` (
+  `id` BIGINT UNSIGNED NOT NULL,
+  `snippetId` BIGINT UNSIGNED NOT NULL,
+  `userId` BIGINT UNSIGNED NOT NULL,
+  `createdAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB;
+
+ALTER TABLE `selectedSnippets`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `snippetId` (`snippetId`),
+  ADD KEY `createdAt` (`createdAt`);
 
 --
 -- AUTO_INCREMENT for tables
@@ -62,4 +87,7 @@ ALTER TABLE `users`
   MODIFY `id` int(4) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 ALTER TABLE `snippets`
+  MODIFY `id` bigint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+ALTER TABLE `selectedSnippets`
   MODIFY `id` bigint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
