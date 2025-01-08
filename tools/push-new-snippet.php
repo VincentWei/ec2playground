68de51413456baa348f8f3dfdb9ce3d156bca6a8
Vincent Wei <vincent@minigui.org>
2025-01-08 14:02:47 +0800

    update

--- tools/push-new-snippet.php
+++ tools/push-new-snippet.php
@@ -5,13 +5,13 @@ require_once("Result.php");
 require_once("HttpUtils.php");
 
 if (!(include "../config/database.php")) {
-    $result = new Result (100, "Bad installation.");
+    $result = new Result(100, "Bad installation.");
     goto error;
 }
 
 if ($_SERVER["REQUEST_METHOD"] != "POST" || empty($_POST["username"])
         || empty($_POST["password"])) {
-    $result = new Result (100, 'Bad request.');
+    $result = new Result(100, '缺少必要请求参数。');
     goto error;
 }
 
@@ -19,28 +19,35 @@ $username = $db->escape_string($_POST['username']);
 $password = $_POST['password'];
 $db_result = $db->query("SELECT id, passwd FROM users WHERE name = '$username'");
 if (!$db_result) {
-    $result = new Result(100, $db->last_error());
+    $result = new Result(100, '数据库错误：' . $db->last_error());
     goto error;
 }
 
 if ($db->num_rows($db_result) == 0) {
-    $result = new Result(100, "No such user.");
-    goto error;
+    $hashed_passwd = $db->escape_string(password_hash(
+                $password, PASSWORD_BCRYPT, ["cost"=>10]));
+    $db_result = $db->query("INSERT INTO users (name, passwd, lastSignIn)
+            VALUES ('$username', '$hashed_passwd', NOW())");
+    if (!$db_result) {
+        $result = new Result(100, "数据库错误：". $db->last_error());
+        goto error;
+    }
 }
+else {
+    $row = $db->fetch_one($db_result);
+    $userId = $row["id"];
+    $hashed = $row["passwd"];
+    $db->free_result($db_result);
 
-$row = $db->fetch_one($db_result);
-$userId = $row["id"];
-$hashed = $row["passwd"];
-$db->free_result($db_result);
-
-if (!password_verify($password, $hashed)) {
-    $result = new Result(100, "Bad password or no such user.");
-    goto error;
+    if (!password_verify($password, $hashed)) {
+        $result = new Result(100, '密码错误。');
+        goto error;
+    }
 }
 
 if (empty($_POST['section']) || empty($_POST['title'])
         || empty($_POST['snippet'])) {
-    $result = new Result(100, "Bad contents.");
+    $result = new Result(100, '类名、标题或者程序为空。');
     goto error;
 }
 
@@ -52,7 +59,7 @@ $res = HttpUtils::httpsGitLabSnippetNew($db->gitlab_host(),
         $db->gitlab_token(), $username, $section, $title, $description, $snippet);
 
 if (!is_array($res)) {
-    $result = new Result (100, "Failed to push snipeet to GitLab server.");
+    $result = new Result (100, "将程序发布到 GitLab 服务器时出现错误。");
     goto error;
 }
 
@@ -67,11 +74,11 @@ $db_result =
 
 if (!$db_result) {
     $result = new Result(100,
-            "Failed to insert new record for the snippet." . $db->last_error());
+            '数据库错误：' . $db->last_error());
     goto error;
 }
 
-$result = new Result(0, 'Success');
+$result = new Result(0, '成功');
 $result->extrMsg = $digest;
 $result->data = $gitlabId;
 
